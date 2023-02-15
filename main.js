8c1a9ab16d3671786699863f998002e1303e0373
Greg <gregmalczynski@gmail.com>
2023-02-15 10:51:07 +0100

    fix / code optimalization

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const cells = document.querySelectorAll('.cell')
 const currentPlayer = document.querySelector('.app-game-current-player')
+const winnerText = document.querySelector('.app-winner')
 
 const rounds = 10;
 let turns = 0;
@@ -11,6 +12,7 @@ let checkWinArrX = []
 let checkWinArrO = []
 
 currentPlayer.innerHTML = 'Turn X'
+winnerText.innerHTML = ''
 
 const checkX = () => {
     const readValuesFromGame = [...cells].map((item) => checkWinArrX.push(item.innerText))
@@ -25,8 +27,12 @@ const checkX = () => {
         (checkWinArrX[2] == 'X' && checkWinArrX[4] == 'X' && checkWinArrX[6] == 'X') 
         ) {
         console.log('X Win!')
+        winnerText.innerHTML = 'Winner is X'
         currentPlayer.innerHTML = 'Game Over'
         window.removeEventListener('click', handleClick)
+        setTimeout(() => {
+            resetGame()
+        }, 3000)
     }
 }
 const checkO = () => {
@@ -42,8 +48,12 @@ const checkO = () => {
         (checkWinArrO[2] == 'O' && checkWinArrO[4] == 'O' && checkWinArrO[6] == 'O')       
         ) {
         console.log('O Win!')
+        winnerText.innerHTML = 'Winner is O'
         currentPlayer.innerHTML = 'Game Over'
         window.removeEventListener('click', handleClick)
+        setTimeout(() => {
+            resetGame()
+        }, 3000)
     }
 }
 
@@ -59,8 +69,16 @@ const handleClick = (e) => {
         } else {
             if( playerX ) {
                 e.target.innerText = 'X'
+                //e.target.classList.remove('o')
+                e.target.classList.add('x')
+                // e.target.style.backgroundColor = 'yellow'
+                // e.target.style.color = 'blue'
             } else {
                 e.target.innerText = 'O'
+                //e.target.classList.remove('x')
+                e.target.classList.add('o')
+                // e.target.style.backgroundColor = 'blue'
+                // e.target.style.color = 'yellow'
             }
             playerX = !playerX
             playerO = !playerO
@@ -77,10 +95,26 @@ const handleClick = (e) => {
         if ( turns == 9 ) {
             currentPlayer.innerHTML = 'Game Over'
             window.removeEventListener('click', handleClick)
+            resetGame()
         }
     }
-    
     console.log(playerX + ' ' + playerO)
+}
+
+const resetGame = () => {
+    [...cells].map((item) => {
+        item.innerText = '-'
+        item.classList.remove('o')
+        item.classList.remove('x')
+        // item.style.backgroundColor = 'rgb(255, 255, 255)'
+        // item.style.color = 'black'
+    })
+    winnerText.innerHTML = ''
+    currentPlayer.innerHTML = 'Turn X'
+    checkWinArrX = []
+    checkWinArrO = []
+    turns = 0
+    window.addEventListener('click', handleClick)
 }
 
 window.addEventListener('click', handleClick)
